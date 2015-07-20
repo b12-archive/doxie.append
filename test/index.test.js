e32060f166843da48707a6b441c21a7a8aeaa6f5
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-07-20 12:14:19 +0200

    Specify programmatic plugin

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,46 @@
+/* jshint -W079 */
+
+import append from '../module/index';
+
+const assign = require('object-assign');
 const test = require('tape-catch');
+const curry = require('1-liners/curry');
+const last = require('1-liners/last');
+const butLast = require('1-liners/butLast');
+const property = curry(require('1-liners/property'));
+const dummyOutput = require('doxie-dummy/output');
+
+test('Programmatic API:  Sticks a string at the end of the docs.', (is) => {
+  const input = dummyOutput([
+    'a',
+    'b',
+  ]);
+
+  const output = append('c')(input);
+
+  is.deepEqual(
+    output.data.map(property('output')),
+    ['a', 'b', 'c'],
+    'pushes the input onto docs'
+  );
+
+  is.deepEqual(
+    last(output.data),
+    {output: 'c'},
+    'not populating any other property'
+  );
+
+  is.deepEqual(
+    assign({}, output, {data: butLast(output.data)}),
+    input,
+    'leaving the rest of the data intact'
+  );
+
+  is.equal(
+    last(append({toString: () => 'd'})(input).data).output,
+    'd',
+    'casts input to string'
+  );
+
+  is.end();
+});
